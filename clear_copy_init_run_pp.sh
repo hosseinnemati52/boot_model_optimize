b421c5b9e017c28f36ddb130f40fb133a1bc877b
Hossein Nemati <nemati52@hotmail.com>
2025-01-28 16:14:02 +0100

    t_tilde_max_adaptor added. It is called in ./clear bla bla bla

--- clear_copy_init_run_pp.sh
+++ clear_copy_init_run_pp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python3 t_tilde_max_adaptor.py
+
 # Read the value of N from the N_runs.csv file
 N=$(<N_runs.csv)
 
@@ -24,21 +26,3 @@ done
 # Run the Python initialization script
 python3 init_cell_number_maker_v2.py
 
-# Run the initialization executable in each directory
-#for i in $(seq $N0 $N); do
-#    cd "run_$i"
-#    ./initialization.sh
-#    cd ..
-#done
-
-#./terminal_opener.sh
-
-
-# Open terminals and run scripts
-#./terminal_opener.sh
-
-# Wait for all terminals to close
-#sleep 4m
-
-# Run the final Python script after all terminals are closed
-#python3 org_pp_over_runs_v5.py
---
+++ t_tilde_max_adaptor.py
@@ -0,0 +1,69 @@
+
+import numpy as np
+
+def indices_finder(string, begin_char, end_char):
+    
+    start_index = -1 + len(begin_char)
+    
+    while 1:
+        start_index += 1
+        
+        if string[start_index-len(begin_char):start_index] == begin_char:
+            
+            end_index = string.index(end_char, start_index)
+            
+            try:
+                value = float(string[start_index:end_index])
+                break
+            except:
+                continue
+        
+    
+    return start_index, end_index
+
+def modify_func(task_info, file_path, new_val):
+    
+    modified_lines = []
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line[:len(task_info['line_word'])] == task_info['line_word']:
+                
+                # start_index = line.index(task_info['begin_char']) + len(task_info['begin_char'])
+                # end_index = line.index(task_info['end_char'])
+                
+                start_index, end_index = indices_finder(line, task_info['begin_char'], task_info['end_char'])
+                
+                modified_line = line[:start_index]+str(task_info['factor']*new_val)+line[end_index:]
+                modified_lines.append(modified_line)
+                
+            else:
+                modified_lines.append(line)
+    
+    with open(file_path, 'w') as file:
+        file.writelines(modified_lines)
+       
+    return 1
+
+with open("compos.txt", "r") as compos:
+    compos_key = compos.readline()[:-1]
+    compos.close()
+
+if compos_key != 'WT':
+
+    t_tilde_eval_file = np.loadtxt("../WT/t_tilde_eval.txt", delimiter=',')
+    t_tilde_eval_avg = t_tilde_eval_file[0]
+    t_tilde_eval_err = t_tilde_eval_file[1]
+    
+    t_tilde_max = t_tilde_eval_avg - (t_tilde_eval_avg%2) + 2
+    
+    modif_task = dict()
+    modif_task['nondim_quantity'] = 't_tilde_max'
+    modif_task['line_word'] = 'maxTime'
+    modif_task['update_switch'] = 1
+    modif_task['begin_char'] = '='
+    modif_task['end_char'] = '\n'
+    modif_task['factor'] = +1
+    
+    modify_func(modif_task, "source/params.csv" , t_tilde_max)
+    
