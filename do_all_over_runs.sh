65abbc97a75fc3add60fc5eb5c01d691008eb09d
Hossein Nemati <nemati52@hotmail.com>
2025-01-29 22:32:38 +0100

    overal_pp is cleared in the beginning. After the overal pp, a pp_done file is generated in overal_pp. terminal opener writes the directory.

---
+++ do_all_over_runs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./clear_copy_init_run_pp.sh
+./terminal_opener.sh
+./pp_plus_cost.sh
--- terminal_opener.sh
+++ terminal_opener.sh
@@ -5,9 +5,9 @@ run_file_in_terminal() {
   subfolder=$1
   file=$2
   title=$3
-
+  
   # Open a new terminal window in the specified subfolder and run the file with a custom title
-  gnome-terminal --working-directory="$PWD/$subfolder" --title="$title" -- sh -c "./$file; exit"
+  gnome-terminal --working-directory="$PWD/$subfolder" --title="$title" -- sh -c "echo "$PWD/$subfolder"; ./$file; exit"
 }
 
 N=$(<N_runs.csv)  # Total number of runs from the CSV file
